ded5f9f747affc47377fc397d50361d0deb827f5
konez2k <konez@protonmail.com>
2020-05-17 01:14:08 +0200

    update tests

--- seeder_test.go
+++ seeder_test.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"github.com/btcsuite/btcd/wire"
 	"net"
 	"strconv"
 	"testing"
+
+	"github.com/btcsuite/btcd/wire"
 )
 
 func TestGetNonStdIP(t *testing.T) {
-
 	var iptests = []struct {
 		rip   string
 		port  uint16
@@ -52,7 +52,7 @@ func TestAddnNa(t *testing.T) {
 			IP:   net.ParseIP(atest.ip),
 			Port: atest.port,
 		}
-		na, _ := wire.NewNetAddress(tcpAddr, 0)
+		na := wire.NewNetAddress(tcpAddr, 0)
 		ndName := net.JoinHostPort(na.IP.String(), strconv.Itoa(int(na.Port)))
 
 		result := s.addNa(na)
@@ -68,7 +68,7 @@ func TestAddnNa(t *testing.T) {
 		IP:   net.ParseIP("127.0.0.1"),
 		Port: 1234,
 	}
-	na, _ := wire.NewNetAddress(tcpAddr, 0)
+	na := wire.NewNetAddress(tcpAddr, 0)
 	result := s.addNa(na)
 
 	if result != false {
@@ -79,15 +79,10 @@ func TestAddnNa(t *testing.T) {
 		IP:   net.ParseIP("1.2.3.4"),
 		Port: 28333,
 	}
-	na, _ = wire.NewNetAddress(tcpAddr, 0)
+	na = wire.NewNetAddress(tcpAddr, 0)
 	result = s.addNa(na)
 
 	if result != false {
 		t.Errorf("node added but should have failed as duplicate: %s", net.JoinHostPort(na.IP.String(), strconv.Itoa(int(na.Port))))
 	}
-
 }
-
-/*
-
- */
